70a415d76eb351a5dcaa8aa178d131db967033c7
xMashu <crazytnt22@gmail.com>
2024-09-26 00:17:43 -0700

    done

--- fib.c
+++ fib.c
@@ -1,6 +1,4 @@
-
 #include <stdio.h>
-
 
 long long int iterative(long long int n)
 {
@@ -40,46 +38,27 @@ long long int recursion(long long int n)
    {
       return recursion(n-1) + recursion(n-2);
    }
-
-
 }
-
-//https://www.gnu.org/software/c-intro-and-ref/manual/html_node/Iterative-Fibonacci.html
-// helped with understanding iterative fib again
-
-
 
 
 
 int main(int argc, char *argv[]) {
 
    int  commandLineInt = 0;
-   sscanf(argv[1], "%d", &commandLineInt);
-   //https://www.geeksforgeeks.org/c-program-for-char-to-int-conversion/
 
-   char fibOption = *argv[2];
-   char *filename = argv[3];
+   sscanf(argv[1], "%d", &commandLineInt); //scan for first input and add to commandLineInt
 
-
-   //scanf("%s", filename);
-   FILE *file = fopen(filename, "r"); 
-   // https://www.tutorialspoint.com/c_standard_library/c_function_fopen.htm
-
-
-   int fileInteger;
-   fscanf(file, "%d", &fileInteger);
-   //https://www.tutorialspoint.com/c_standard_library/c_function_fscanf.htm
-   fclose(file);
+   char fibOption = *argv[2]; // if recursive or iterative
 
    long long int n = 0;
-   n = commandLineInt + fileInteger -1; 
+   n = commandLineInt -1; 
    // -1 cuz fib has to start at 0
 
 
 
 
-   long long int result = 0;
- 
+   long long int result = 0; //set to 0 so if no response it'll just default to 0
+
    if(fibOption == 'i')
    {
       result = iterative(n);
@@ -90,9 +69,5 @@ int main(int argc, char *argv[]) {
    }
 
    printf("%lld\n", result);
-
-
-
-
 
 }
